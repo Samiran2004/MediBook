3e38a6ac262568e7d803a616b286d273d407c60c
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-03-06 17:55:57 +0530

    starter code for project

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+//view engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
