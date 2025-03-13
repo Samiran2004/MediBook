8e806e3a08fefec1763df65c9b393c0b87a157cf
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2025-03-13 10:15:49 +0530

    add/Delete doctor by doctor's id<Samiran2004>

---
+++ controllers/delete/deleteDoctorById.controller.js
@@ -0,0 +1,33 @@
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
+import Models from "../../models/index.models";
+
+const deleteDoctorByIdController = async (req, res) => {
+    try {
+        const { doctorid } = req.body;
+        if (!doctorid) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'Failed',
+                message: "Please provide the doctor's id!"
+            });
+        }
+        try {
+            await Models.DoctorModel.findByIdAndDelete(doctorid);
+            return res.status(StatusCodes.OK).json({
+                status: 'OK',
+                message: "Doctor deleted!"
+            });
+        } catch (error) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'Failed',
+                message: `${doctorid} is not a valid doctor's id!`
+            });
+        } ``
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            status: 'Failed',
+            message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
+        });
+    }
+}
+
+export default deleteDoctorByIdController;
